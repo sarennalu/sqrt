e897af8024cae395959e3a863e4571cebceda1e7
sulman01 <sulman01@student.ubc.ca>
2026-01-15 13:15:19 -0800

    fixing number

--- sqrt.py
+++ sqrt.py
@@ -9,7 +9,7 @@ import math
 @click.command()
 @click.option("--n", type=int, required=True, help="Number for which the square root should be calculated")
 def main(n):
-    if number < 0:
+    if n < 0:
         raise Exception("n should not a positive number")
     print(math.sqrt(n))
 
